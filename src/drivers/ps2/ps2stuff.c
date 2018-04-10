4b4821f9566bd8abf989611ad1c58ae2ac7a4ff7
infvaL <38145742+infval@users.noreply.github.com>
2018-04-10 03:12:26 +0300

    Refactoring

--- src/drivers/ps2/ps2stuff.c
+++ src/drivers/ps2/ps2stuff.c
@@ -3,32 +3,32 @@
 #include "../../driver.h"
 
 struct pcpal {
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
 } pcpalette[256];
 extern unsigned int ps2palette[256];
 
 //main
 FILE *FCEUD_UTF8fopen(const char *fn, const char *mode)
 {
-	return(fopen(fn,mode));
+    return(fopen(fn,mode));
 }
 
 //Functions to display messages differently
 void FCEUD_PrintError(char *s)
 {
-	printf("Error %s", s);
+    printf("Error %s", s);
 }
 
 void FCEUD_Message(char *s)
 {
-	printf("%s", s);
+    printf("%s", s);
 }
 
 void FCEUD_SetEmulationSpeed(int cmd)
 {
-	printf("SetEmulationSpeed not implemented.\n");
+    printf("SetEmulationSpeed not implemented.\n");
 }
 
 //Sound related functions
@@ -36,69 +36,69 @@ void FCEUD_SetEmulationSpeed(int cmd)
 static int soundvolume=1024;
 void FCEUD_SoundVolumeAdjust(int adjust)
 {
-	switch(adjust)
-	{
-	case -1:  soundvolume-=50; if(soundvolume<0) soundvolume=0; break; //lower volume
-	case 0:	  soundvolume=1024; break;
-	case 1:	  soundvolume+=50; if(soundvolume>1024) soundvolume=1024; break; //raise volume
-	}
-	FCEUI_SetSoundVolume(soundvolume);
-	FCEU_DispMessage("Sound volume %d.", soundvolume);
+    switch(adjust)
+    {
+    case -1: soundvolume-=50; if(soundvolume<0) soundvolume=0; break; //lower volume
+    case  0: soundvolume=1024; break;
+    case  1: soundvolume+=50; if(soundvolume>1024) soundvolume=1024; break; //raise volume
+    }
+    FCEUI_SetSoundVolume(soundvolume);
+    FCEU_DispMessage("Sound volume %d.", soundvolume);
 }
 
 int mute = 0;
 void FCEUD_SoundToggle(void)
 {
-	if(mute)
-	{
-		mute=0;
-		FCEUI_SetSoundVolume(soundvolume);
-		FCEU_DispMessage("Sound mute off.");
-	}
-	else
-	{
-		mute=1;
-		FCEUI_SetSoundVolume(0);
-		FCEU_DispMessage("Sound mute on.");
-	}
+    if(mute)
+    {
+        mute=0;
+        FCEUI_SetSoundVolume(soundvolume);
+        FCEU_DispMessage("Sound mute off.");
+    }
+    else
+    {
+        mute=1;
+        FCEUI_SetSoundVolume(0);
+        FCEU_DispMessage("Sound mute on.");
+    }
 }
 
 //Network
 int FCEUD_SendData(void *data, uint32 len)
 {
-	printf("Send Network Data");
-	return(0);
+    printf("Send Network Data");
+    return(0);
 }
 
 void FCEUD_NetworkClose(void)
 {
-	printf("Close Network");
+    printf("Close Network");
 }
 
 int FCEUD_RecvData(void *data, uint32 len)
 {
-	printf("Receive Network Data");
-	return(0);
+    printf("Receive Network Data");
+    return(0);
 }
 
 void FCEUD_NetplayText(uint8 *text)
 {
-	printf("Network Text");
+    printf("Network Text");
 }
 
 //Video
 void FCEUD_SetPalette(uint8 index, uint8 r, uint8 g, uint8 b)
 {
-	pcpalette[index].r = r;
-	pcpalette[index].g = g;
-	pcpalette[index].b = b;
+    pcpalette[index].r = r;
+    pcpalette[index].g = g;
+    pcpalette[index].b = b;
 }
 
 void FCEUD_GetPalette(uint8 i,uint8 *r, uint8 *g, uint8 *b)
 {
-	*r = pcpalette[i].r;
-	*g = pcpalette[i].g;
-	*b = pcpalette[i].b;
+    *r = pcpalette[i].r;
+    *g = pcpalette[i].g;
+    *b = pcpalette[i].b;
 }
 
 
